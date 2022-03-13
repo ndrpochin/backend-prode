95a639872da63dd0171a9530ceff3e274991e821
Nadir Eliel Jaramillo Rosas <nadirjaramillorosas@hotmail.com>
2022-03-13 10:38:10 -0300

    second commit

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const app = express();
-app.listen(3000, () => {
-  console.log("El servidor está inicializado en el puerto 3000");
-});
 
-app.get("/", function (req, res) {
+app.get("/resultado", function (req, res) {
   //res.send('Saludos desde express');
   let obj = {
     fechayhora: "2022-03-11 12:00:00",
+    fase: "grupos",
     pais_local: "Argentina",
     pais_visitante: "Qatar",
     goles_local: 3,
@@ -18,4 +16,29 @@ app.get("/", function (req, res) {
     },
   };
   res.send(obj);
+});
+
+
+//LOGIN & LOGOUT
+app.post('/login', function (req, res) {
+  res.send('Loggueado');
+})
+
+app.post('/logout', function (req, res) {
+  res.send('deslogueado');
+})
+
+
+
+//TEST
+app.post('/test', function (req, res) {
+  res.send('[POST] Saludos desde express');
+});
+
+app.get('/test', function (req, res) {
+  res.send('[GET] Saludos desde express');
+});
+
+app.listen(3000, () => {
+  console.log("El servidor está inicializado en el puerto 3000");
 });
